fc5c80d6c35611d60589b7ac14af23e967609585
Chris Reynolds <chris@jazzsequence.com>
2014-10-28 09:44:34 -0700

    version bump

--- book-review-library.php
+++ book-review-library.php
@@ -10,7 +10,7 @@
  * Plugin Name: Book Review Library
  * Plugin URI:  http://museumthemes.com/book-review-library/
  * Description: A book cataloguing and review system designed with bookophiles and librarians in mind.
- * Version:     1.4.11
+ * Version:     1.4.12
  * Author:      Chris Reynolds
  * Author URI:  http://chrisreynolds.io
  * License:     GPL3
