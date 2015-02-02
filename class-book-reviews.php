1e97d00673d26da9c95f5eddc4933d2b663fc399
Chris Reynolds <chris@jazzsequence.com>
2015-02-02 10:37:27 -0700

    include options.php

--- class-book-reviews.php
+++ class-book-reviews.php
@@ -64,6 +64,7 @@ class Book_Reviews {
 	private function __construct() {
 
 		require_once( plugin_dir_path( __FILE__ ) . '/views/actions.php' );
+		require_once( plugin_dir_path( __FILE__ ) . '/views/options.php' );
 		require_once( plugin_dir_path( __FILE__ ) . '/inc/cmb.php' );
 		require_once( plugin_dir_path( __FILE__ ) . '/inc/cpt.php' );
 		require_once( plugin_dir_path( __FILE__ ) . '/inc/taxonomy.php' );
