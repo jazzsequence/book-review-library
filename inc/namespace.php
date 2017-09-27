2a471ae0dcecc46dec29a9f9d1b837a44ce42e04
Chris Reynolds <chris@jazzsequence.com>
2017-09-27 11:47:20 -0600

    new hm-generated plugin

---
+++ inc/namespace.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Book Review Library
+ */
+
+namespace BookReview\Library;
+
+/**
+ * Bootstrap the plugin.
+ *
+ * Registers actions and filter required to run the plugin.
+ */
+function bootstrap() {
+	spl_autoload_register( __NAMESPACE__ . '\\autoload' );
+
+	// Add all your plugin hooks here.
+}
+
+/**
+ * Autoload classes for this namespace.
+ *
+ * @param string $class Class name.
+ */
+function autoload( $class ) {
+	if ( strpos( $class, __NAMESPACE__ . '\\' ) !== 0 ) {
+		return;
+	}
+
+	$relative = strtolower( substr( $class, strlen( __NAMESPACE__ . '\\' ) ) );
+	$parts = explode( '\\', $relative );
+	$final = array_pop( $parts );
+	array_push( $parts, 'class-' . $final . '.php' );
+	$path = __DIR__ . '/' . implode( '/', $parts );
+
+	require $path;
+}
---
+++ plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Book Review Library
+ * Plugin URI: https://wordpress.org/plugins/book-review-libarary/
+ * Description: A book cataloguing and review system designed with book collectors, bookophiles and librarians in mind.
+ * Author: Chris Reynolds
+ * Author URI: https://chrisreynolds.io
+ * License: GPLv2
+ */
+
+namespace BR\BookReview;
+
+require_once __DIR__ . '/inc/namespace.php';
+
+// Kick it off.
+add_action( 'plugins_loaded', __NAMESPACE__ . '\\bootstrap' );
