2a9af74549e77c6f6d50a43d22549c061e521d68
Chris Reynolds <chris@jazzsequence.com>
2017-09-27 17:47:17 -0600

    adds a namespace for the cpt

---
+++ inc/cpt/namespace.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Book Review Post Type
+ *
+ * Registers and handles the Book Review post type.
+ *
+ * @since 2.0.0-alpha
+ *
+ * @package BookReview
+ */
+
+namespace BookReview\CPT;
