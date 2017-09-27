175930512cc409a70e91667d1e92da1da5535312
Chris Reynolds <chris@jazzsequence.com>
2017-09-27 17:32:12 -0600

    migrate code from old main plugin file to new

--- plugin.php
+++ plugin.php
@@ -12,5 +12,12 @@ namespace BookReview;
 
 require_once __DIR__ . '/inc/namespace.php';
 
+// TODO: Find a better way to solve this problem.
+$is_book_review_shortcode = false;
+
 // Kick it off.
 add_action( 'plugins_loaded', __NAMESPACE__ . '\\bootstrap' );
+
+// Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
+register_activation_hook( __FILE__,   __NAMESPACE__ . '\\activate' );
+register_deactivation_hook( __FILE__, __NAMESPACE__ . '\\deactivate' );
