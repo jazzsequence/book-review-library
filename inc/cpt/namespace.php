b221df7c810eaad0d715c5d5fce41dadf37b22f3
Chris Reynolds <chris@jazzsequence.com>
2017-10-10 19:33:32 +0200

    first pass at function to register the post type

--- inc/cpt/namespace.php
+++ inc/cpt/namespace.php
@@ -10,3 +10,40 @@
  */
 
 namespace BookReview\CPT;
+
+function register_book_reviews() {
+	$defaults = \book_reviews_option_defaults();
+	$supports = [
+		'title',
+		'editor',
+		'author',
+		'thumbnail',
+		'revisions',
+		'comments',
+	];
+
+	// Remove comment support if we aren't enabling comments.
+	if ( ! book_reviews_is_option_enabled( 'comments' ) ) {
+		unset( $supports['commetns'] );
+	}
+
+	$capabilities = [
+		'publish_posts'     => 'publish_book-reviews',
+		'edit_posts'        => 'edit_book-reviews',
+		'edit_others_posts' => 'edit_others_book-reviews',
+		'delete_posts'      => 'delete_book-reviews',
+		'edit_post'         => 'edit_book-review',
+		'delete_post'       => 'delete_book-review',
+		'read_post'         => 'read_book-review',
+	];
+
+	register_extended_post_type( 'book-review', [
+		'show_in_feed'    => false, // This should be an option.
+		'admin_cols'      => [],
+		'admin_filters'   => [],
+		'menu_position'   => 20,
+		'capability_type' => 'book-review',
+		'capabilities'    => $capabilities,
+		'map_meta_cap'    => true,
+	] );
+}
