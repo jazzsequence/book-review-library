b3de0ecd7d6bb3c250b300a8e0e670475ec2b886
Chris Reynolds <chris@jazzsequence.com>
2017-12-08 10:46:49 -0700

    add a new namespace for the shortcode stuff

---
+++ inc/shortcode/namespace.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Shortcode
+ *
+ * Handles the Book Review Library shortcode functionality.
+ *
+ * @since 2.0.0-20171208
+ *
+ * @package BookReview
+ */
+
+namespace BookReview\Shortcode;
+use BookReview\Options as Options;
+use BookReview\CMB2 as CMB2;
--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Book Review Library
  * Plugin URI: https://wordpress.org/plugins/book-review-libarary/
  * Description: A book cataloguing and review system designed with book collectors, bookophiles and librarians in mind.
- * Version: 2.0.0-20171207
+ * Version: 2.0.0-20171208
  * Author: Chris Reynolds
  * Author URI: https://chrisreynolds.io
  * License: GPLv2
@@ -24,6 +24,7 @@ require_once __DIR__ . '/inc/cmb2/namespace.php';
 require_once __DIR__ . '/inc/roles/namespace.php';
 require_once __DIR__ . '/inc/taxonomies/namespace.php';
 require_once __DIR__ . '/inc/options/namespace.php';
+require_once __DIR__ . '/inc/shortcode/namespace.php';
 require_once __DIR__ . '/inc/template-tags.php';
 
 // TODO: Find a better way to solve this problem.
